5c9235046a1ce8964cf6ed0b39c3d44a28d94822
matheuseabra <matheuseabra@gmail.com>
2018-11-10 10:17:49 -0200

    Initial commit

---
+++ _assets/js/_inits.js
@@ -0,0 +1,94 @@
+// ----------------------------------------------
+// Imports
+// ----------------------------------------------
+import anime from 'animejs';
+import AOS from 'aos';
+import Barba from 'barba.js';
+import Rellax from 'rellax';
+import SmoothScroll from 'smooth-scroll';
+import InfiniteScroll from './components/_infiniteScroll.js';
+import { miscAnchor, miscCycle, miscNavigation } from './components/_miscellaneous.js';
+import NavigationScroll from './components/_navigationScroll.js';
+import OpacityScroll from './components/_opacityScroll.js';
+
+// ----------------------------------------------
+// Inits
+// ----------------------------------------------
+$(() => {
+
+  // Barba
+  const SlideTransition = Barba.BaseTransition.extend({
+    start() {
+      Promise.all([this.newContainerLoading, this.slideOut(), new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 700);
+      })]).then(this.slideIn.bind(this));
+    },
+
+    slideOut() {
+      $('.transition').addClass('transition-out');
+
+      return new Promise(resolve => {
+        anime({
+          targets: '.transition',
+          translateX: '-=100%',
+          easing: 'easeInQuart',
+          duration: 350,
+          complete() {
+            resolve();
+          }
+        });
+      });
+    },
+
+    slideIn() {
+      $('.transition').removeClass('transition-out').addClass('transition-in');
+
+      window.scrollTo(0, 0);
+      this.oldContainer.style.display = 'none';
+      this.newContainer.style.visibility = 'visible';
+
+      this.done();
+    }
+  });
+
+  Barba.Dispatcher.on('transitionCompleted', () => {
+    $('body').removeClass('js-scrolling-down js-scrolling-up');
+
+    anime({
+      targets: '.transition',
+      translateY: '-100%',
+      easing: 'easeOutQuart',
+      duration: 350,
+      delay: 200,
+      complete() {
+        $('.transition').removeClass('transition-in');
+      }
+    });
+
+    AOS.init({
+      duration: 1000,
+      easing: 'ease',
+      once: true
+    });
+
+    const rellax = new Rellax('.rellax');
+    const scroll = new SmoothScroll('a[href*="#"]');
+
+    NavigationScroll.init();
+    OpacityScroll.init();
+    miscAnchor();
+    miscCycle();
+    miscNavigation();
+
+    if ($('.posts').length && $('.posts__next').length) {
+      InfiniteScroll.init();
+    }
+  });
+
+  Barba.Pjax.getTransition = () => SlideTransition;
+
+  Barba.Pjax.start();
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const cleanCSS = require('gulp-clean-css');
+const eslint = require('gulp-eslint');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+gulp.task('sass', () => {
+  return gulp.src('./_assets/scss/app.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+  .pipe(cleanCSS())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('./_assets/scss/fonts.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./assets/css'));
+});
+
+// gulp.task('instafetch.js', () => {
+//   return gulp.src('./_assets/scss/instafetch.js.scss')
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+//   .pipe(cleanCSS())
+//   .pipe(rename({suffix: '.min'}))
+//   .pipe(gulp.dest('./assets/css'));
+// });
+
+gulp.task('please-dont-go', () => {
+  return gulp.src('./_assets/scss/please-dont-go.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+  .pipe(cleanCSS())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./assets/css'));
+});
+
+// gulp.task('trophy', () => {
+//   return gulp.src('./_assets/scss/trophy.scss')
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+//   .pipe(cleanCSS())
+//   .pipe(rename({suffix: '.min'}))
+//   .pipe(gulp.dest('./assets/css'));
+// });
+
+gulp.task('lint', () => {
+  return gulp.src([
+    './_assets/js/components/_infiniteScroll.js',
+    './_assets/js/components/_miscellaneous.js',
+    './_assets/js/components/_navigationScroll.js',
+    './_assets/js/components/_opacityScroll.js',
+    './_assets/js/_inits.js'
+  ])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('browserify', () => {
+  return browserify('./_assets/js/app.js')
+  .transform('babelify', {presets: ['env']})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('build', ['sass', 'browserify']);
+
+gulp.task('watch', () => {
+  gulp.watch('./_assets/scss/**/*.scss', ['sass']);
+  gulp.watch('./_assets/js/**/*.js', ['browserify']);
+});
+
+gulp.task('default', ['build', 'watch']);
